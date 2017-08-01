f0d68abe63096c69dd0a6334458275efd0a8e6af
Joseph Long <jlong@stsci.edu>
2017-08-01 14:19:03 -0400

    Add run.sh to git

---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+SCRIPTDIR="$(dirname "$0")"
+docker build -t galsim-binder .
+docker run --mount type=bind,src=$SCRIPTDIR/notebooks,dst=/home/jovyan/notebooks -p 127.0.0.1:8888:8888 galsim-binder
